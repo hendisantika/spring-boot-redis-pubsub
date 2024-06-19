e4d61b60673c5e8fd5153ef77f17a0d114290beb
hendisantika <hendisantika@yahoo.co.id>
2024-06-19 11:14:56 +0700

    Add redisContainer in RedisSubscriberConfiguration🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/redispubsub/config/RedisSubscriberConfiguration.java
+++ src/main/java/id/my/hendisantika/redispubsub/config/RedisSubscriberConfiguration.java
@@ -1,9 +1,13 @@
 package id.my.hendisantika.redispubsub.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +25,17 @@ public class RedisSubscriberConfiguration {
 
     private final MessageListener messageListener;
     private final RedisConnectionFactory connectionFactory;
+
+    @Bean
+    public MessageListenerAdapter messageListenerAdapter() {
+        return new MessageListenerAdapter(messageListener);
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisContainer(ChannelTopic topic) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(messageListener, topic);
+        return container;
+    }
 }
