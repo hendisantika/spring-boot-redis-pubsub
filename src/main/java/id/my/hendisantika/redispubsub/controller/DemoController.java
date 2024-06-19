47055664c223f935c79648fa67a3d3db6338fd86
hendisantika <hendisantika@yahoo.co.id>
2024-06-19 12:20:40 +0700

    Add publish in DemoController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/redispubsub/controller/DemoController.java
+++ src/main/java/id/my/hendisantika/redispubsub/controller/DemoController.java
@@ -1,8 +1,15 @@
 package id.my.hendisantika.redispubsub.controller;
 
+import id.my.hendisantika.redispubsub.model.OrderEvent;
 import id.my.hendisantika.redispubsub.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,4 +28,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private final OrderService orderService;
+
+    @PostMapping("/publish")
+    @Operation(
+            summary = "Add New Order Data",
+            description = "Add New Order Data.",
+            tags = {"Order"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            String.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
+    public String publish(@RequestBody OrderEvent orderEvent) {
+        orderService.publish(orderEvent);
+        return "Success";
+    }
 }
