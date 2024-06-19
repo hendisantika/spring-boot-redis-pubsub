c646807dc67f2e010361311ce855532de12b1f3c
hendisantika <hendisantika@yahoo.co.id>
2024-06-19 11:08:44 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/redispubsub/SpringBootRedisPubsubApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.redispubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisPubsubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisPubsubApplication.class, args);
+    }
+
+}
