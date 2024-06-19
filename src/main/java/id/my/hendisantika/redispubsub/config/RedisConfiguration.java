7602f727b334e8c891823bbe34848d4224bc9a07
hendisantika <hendisantika@yahoo.co.id>
2024-06-19 11:13:21 +0700

    Add RedisConfiguration 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/redispubsub/config/RedisConfiguration.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.redispubsub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-redis-pubsub
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/19/24
+ * Time: 11:13
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class RedisConfiguration {
+
+    @Bean(name = "customRedisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
